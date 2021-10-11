9b3e78945b1a450426dca4421fb27b0a8391ab92
Matthias Bathke <matthias-bathke@straightvisions.com>
2021-10-11 20:17:29 +0200

    FIX STYLING

--- lib/css/config/sidebars.php
+++ lib/css/config/sidebars.php
@@ -17,7 +17,7 @@ for($i = 1; $i < 3; $i++){
 	if($module->get_setting('sidebar_' . $i . '_alignment')->get_data()){
 
 		// outer stuff
-		$properties['justify-self'] = array(); // row
+		$properties['justify-content'] = array(); // row
 		$properties['align-self'] 	= array(); // column
 		$properties['margin-left'] 	= array();
 		$properties['margin-right'] = array();
@@ -25,7 +25,7 @@ for($i = 1; $i < 3; $i++){
 		foreach($prepared_properties[$i] as $key => $value) {
 			$properties['margin-left'][$key]	= '0';
 			$properties['margin-right'][$key]	= '0';
-			$properties['justify-self'][$key]	= 'unset';
+			$properties['justify-content'][$key]	= 'unset';
 			$properties['align-self'][$key]		= 'unset';
 
 			// flex hacks to simulate parent justify content and add more options
@@ -44,12 +44,12 @@ for($i = 1; $i < 3; $i++){
 					$properties['margin-right'][$key] 	= 'auto';
 				}
 
-				$properties['justify-self'][$key] = $value;
+				$properties['justify-content'][$key] = $value;
 			}
 
 			// column direction doesn't need any hacks ;)
 			if( isset($module->get_setting('direction')->get_data()[$key]) && $module->get_setting('direction')->get_data()[$key] === 'column' ) {
-				$properties['align-self'][$key] 	= $value;
+				$properties['justify-content'][$key] 	= $value;
 				$properties['margin-left'][$key] 	= '0';
 				$properties['margin-right'][$key] 	= '0';
 			}
@@ -59,7 +59,7 @@ for($i = 1; $i < 3; $i++){
 	}
 
 	echo $_s->build_css(
-		'.sv100_sv_header_bar_wrapper .sv100_sv_header_bar_inner > .sv100_sv_sidebar_sv_header_bar_' . $i,
+		'.sv100_sv_header_bar_wrapper .sv100_sv_header_bar_inner .sv100_sv_sidebar_header_bar_' . $i,
 		$properties
 	);
 }
